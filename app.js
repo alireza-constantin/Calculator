c77b9d4c2c80a891ee1e85e9c23579f3a9d27407
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2021-10-24 19:48:50 +0330

    compeleted

--- app.js
+++ app.js
@@ -2,83 +2,114 @@ const operators = document.querySelectorAll('.op');
 const panel = document.getElementById('panel');
 const prev = document.getElementById('prevDis');
 const curr = document.getElementById('currDis');
+const modal = document.querySelector('.modal');
 
-let prevVal = 0;
-let currVal = 0;
-let answer = 0;
-let operator = '';
+let prevVal = '';
+let currVal = '';
+let answer = '';
+let operation;
 
-let first = true;
+const history = [];
 
 panel.addEventListener('click', (e) => {
 	const target = e.target;
 	const text = target.innerText;
 
-	currVal = +curr.innerText;
-
 	if (target.classList.contains('op')) {
-		if (text !== '=') {
-			operator = text;
-		}
-		switch (text) {
-			case '+':
-				if (currVal === answer) {
-					return;
-				}
-				answer = +prevVal + +currVal;
-				break;
-			case '-':
-				console.log('manfi');
-				if (currVal === answer) {
-					return;
-				}
-				answer = +prevVal - +currVal;
-				break;
-			case '×':
-				if (currVal === answer) {
-					return;
-				}
-				first ? (prevVal = 1) : prevVal;
-				answer = +prevVal * +currVal;
-				first = false;
-				break;
-			case '÷':
-				if (currVal === answer) {
-					return;
-				}
-				console.log('hi');
-				// first ? (prevVal = 1) : prevVal;
-				answer = +prevVal / +currVal;
-				first = false;
-				break;
-			case '=':
-				if (operator === '+') {
-					answer = +prevVal + +currVal;
-					operator = '+';
-				} else if (operator === '-') {
-					answer = +prevVal - +currVal;
-					operator = '-';
-				} else if (operator === '×') {
-					answer = +prevVal * +currVal;
-					operator = '×';
-				} else if (operator === '÷') {
-					answer = +prevVal / +currVal;
-					operator = '÷';
-				}
-
-			default:
-				break;
-		}
-
-		prevVal = answer;
-		currVal = answer;
+		chooseOperation(text);
+		updateUI();
+	} else if (target.classList.contains('equal')) {
+		calculate();
+		updateUI();
+	} else if (target.classList.contains('clear')) {
+		clear();
+		updateUI();
+	} else if (target.classList.contains('delete')) {
+		backSpace();
+		updateUI();
+	} else if (target.classList.contains('history')) {
+		modal.classList.add('show');
+		createHistory();
 	} else {
-		if (currVal === answer) {
-			currVal = 0;
-		}
+		if (currVal.includes('.') && text === '.') return;
 		currVal += text;
+		updateUI();
+	}
+});
+
+function updateUI() {
+	curr.innerText = currVal;
+	if (operation != null) {
+		prev.innerText = `${prevVal} ${operation}`;
+	} else {
+		prev.innerText = '';
+	}
+}
+
+function chooseOperation(text) {
+	if (currVal === '') return;
+	if (prevVal !== '') calculate();
+	operation = text;
+	prevVal = currVal;
+	currVal = '';
+}
+
+function clear() {
+	currVal = '';
+	prevVal = '';
+	operation = '';
+}
+
+function backSpace() {
+	currVal = currVal.toString().slice(0, -1);
+}
+
+function calculate() {
+	let answer;
+	const prev = +prevVal;
+	const curr = +currVal;
+
+	// Checking if the values exists
+	if (isNaN(prev) || isNaN(curr)) return;
+
+	switch (operation) {
+		case '+':
+			answer = prev + curr;
+			break;
+		case '-':
+			answer = prev - curr;
+			break;
+		case '÷':
+			answer = prev / curr;
+			break;
+		case '×':
+			answer = prev * curr;
+			break;
+		default:
+			return;
 	}
 
-	curr.innerText = +currVal;
-	prev.innerText = `${prevVal} ${operator}`;
+	history.push(`${prevVal} ${operation} ${currVal} = ${answer}`);
+
+	currVal = answer;
+	operation = undefined;
+	prevVal = '';
+}
+
+function createHistory() {
+	const root = document.getElementById('root');
+	root.innerHTML = '';
+	history.forEach((item) => {
+		const p = document.createElement('p');
+		p.innerText = item;
+		root.append(p);
+	});
+}
+
+modal.addEventListener('click', function (e) {
+	e.stopPropagation();
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+	modal.classList.remove('show');
 });
